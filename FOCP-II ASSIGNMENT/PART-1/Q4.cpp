2f07c4f8763c7c7e07b23c6a958b04d58e3d6154
ISHAN142005 <ishanbohra1414@gmail.com>
2026-01-07 12:39:12 +0530

    Q4

---
+++ FOCP-II ASSIGNMENT/PART-1/Q4.cpp
@@ -0,0 +1,18 @@
+/*
+4.Take two floating-point numbers as input 
+and display their division result with two decimal places.
+*/
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+float a,b;
+cout<<"Enter the first number :";
+cin>>a;
+cout<<"Enter the first number :";
+cin>>b;
+cout<<"The quotient of both the numbers is:"<<fixed<<setprecision(2)<<a/b<<endl;
+
+
+    return 0;
+}
