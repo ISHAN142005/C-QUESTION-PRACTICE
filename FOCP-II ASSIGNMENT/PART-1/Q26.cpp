2936c6cb6b76a2445fd7938acb9b29461cbfcf82
ISHAN142005 <ishanbohra1414@gmail.com>
2026-01-15 22:46:44 +0530

    Q26

---
+++ FOCP-II ASSIGNMENT/PART-1/Q26.cpp
@@ -0,0 +1,38 @@
+/*
+26.Write a program to generate and display the first
+n prime numbers using a combination of for and while loops.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter how many prime numbers to display: ";
+    cin >> n;
+
+    int count = 0;
+    int num = 2;
+
+    while (count < n)
+    {
+        bool isPrime = true;
+        for (int i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+        {
+            cout << num << " ";
+            count++;
+        }
+        num++;
+    }
+
+    cout << endl;
+    return 0;
+}
